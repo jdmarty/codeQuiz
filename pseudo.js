dfd77105659f05d11861e43116ba4bba59d268ac
JD Marty <joshua.dmarty@gmail.com>
2020-10-07 16:49:58 -0700

    added basic HTML to work with script

---
+++ pseudo.js
@@ -0,0 +1,10 @@
+//Features
+
+//Array of objects to store data about a question
+    //String question
+    //array of answers
+    //index of correct answer
+//Function to find an index in that array and generate a question and answers
+//Function to start a timer that loses time when a wrong answer is given
+//Function to store final score and an input name
+//Function to recall stored names and scores and print them to a list
