f2ce12751d6c56c07e8eafbe0022f2eef7a7d140
JD Marty <joshua.dmarty@gmail.com>
2020-10-07 19:17:45 -0700

    added buttons row and function to cycle questions

--- script.js
+++ script.js
@@ -1,51 +1,93 @@
-//questions array
-var htmlQuestions = [
+$(document).ready(function () {
+  //-------------------------------------------------------------------------
+
+  //questions array
+  var htmlQuestions = [
     {
-        question: 'Which of these elements displays inline by default?',
-        answers: [
-            'p', 
-            'div', 
-            'span', 
-            'h1'],
-        correct: 3
+      question: "Which of these elements displays inline by default?",
+      answers: ["p", "div", "span", "h1"],
+      correct: 2,
     },
     {
-        question: 'What is the correct way to add a 2px margin to a div using inline styling?',
-        answers: [
-            '<div style="margin: 2px"</div', 
-            '<div>style=margin: 2px</div',
-            '<div margin="2px"</div',
-            '<div style="2px"></div'
-        ],
-        correct: 1
+      question:
+        "What is the correct way to add a 2px margin to a div using inline styling?",
+      answers: [
+        '<div style="margin: 2px"</div>',
+        '<div>style=margin: 2px</div',
+        '<div margin="2px"</div',
+        '<div style="2px"></div',
+      ],
+      correct: 0,
     },
     {
-        question: 'Which of these is a tag that will make text appear bold?',
-        answers: [
-            '<bold>', 
-            '<em>',
-            '<div class="bold">',
-            '<strong>'
-        ],
-        correct: 4
-    }
-];
+      question: "Which of these is a tag that will make text appear bold?",
+      answers: ["<bold>", "<em>", '<div class="bold">', "<strong>"],
+      correct: 3,
+    },
+  ];
 
-//standard divs
-var $answersBox = $('.answersBox');
-var $answerCard = $('<div class="card text-center my-2">');
-var $answerCardBody = $('<div class="card-body">');
+  //global variables
+  var questions = null;
+  var qNumber = 0;
+  var score = 0;
+  var $questionBox = $("#questionBox");
+  var $answersBox = $("#answersBox");
+  var $buttonBox = $("#buttonBox");
 
-//function to update the questions box
-function updateQuestion(questions, qNumber) {
-    $('#questionBox').html(`<p class="lead">${questions[qNumber].question}</p>`);
+  //function to update the questions box
+  function loadQuestion(questions, qNumber) {
+    var currentQuestion = questions[qNumber];
+    //hide the buttons
+    $buttonBox.hide();
+    //display the answers
+    $answersBox.show();
+    //update the questions box
+    $questionBox.html(`<p class="lead">${currentQuestion.question}</p>`);
+    //empty out the answers box
     $answersBox.empty();
-    for (let answer of questions[qNumber].answers) {
-        let newAnswerCard = $('<div class="card text-center my-2">');
+    for (var i=0; i < currentQuestion.answers.length; i++) {
+        //create a new div with appropriate classes
+        var newAnswerCard = $('<div class="card text-center my-2 answer">');
+        //give that div an event listener
+        newAnswerCard.on("click", nextQuestion);
+        //append it to the answers
         $answersBox.append(newAnswerCard);
-        let newAnswerCardBody = $(`<div class="card-body">${answer}</div>`);
+        //create a new div for the text
+        var newAnswerCardBody = $(`<div class="card-body"></div>`)
+        //set the text
+        newAnswerCardBody.text(currentQuestion.answers[i]);
+        //append it to the new card
         newAnswerCard.append(newAnswerCardBody);
     }
-};
+  }
 
-updateQuestion(htmlQuestions, 0);
+  //function to load in the appropriate quiz
+  $("#buttonBox").on("click", function (e) {
+    if (!e.target.matches("button")) return;
+    var quizType = e.target.id;
+    switch (quizType) {
+      case "htmlStart":
+        questions = htmlQuestions;
+        break;
+      case "cssStart":
+        questions = htmlQuestions;
+        break;
+      case "jsStart":
+        questions = htmlQuestions;
+        break;
+      case "jqStart":
+        questions = htmlQuestions;
+        break;
+    }
+    loadQuestion(questions, qNumber);
+  });
+
+  //function to load the next question
+  function nextQuestion() {
+      qNumber++
+      if (qNumber < questions.length) loadQuestion(questions, qNumber);
+  }
+
+  //-----------------------------------------------------------------------------------------
+});
+
